ed2d8da98e32c734ff57c6e4a7a6df76c1addbb2
furvietto <fulvio-94@hotmail.it>
2022-03-14 15:00:53 +0100

    add contact home and about.vue

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,9 +1,38 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
 import App from './views/App';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes:[
+        {
+            path:'/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path:'/about',
+            name: 'about',
+            component: About
+        },
+        {
+            path:'/contact',
+            name: 'contact',
+            component: Contact
+        }
+    ]
+});
 
 const app = new Vue({
     el: '#app',
     render: h => h(App),
+    router
 });
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,15 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::middleware("auth")
+->namespace("Admin")
+->name("admin.")
+->prefix("admin")
+->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    });
 
 Route::get("{any?}", function () {
     return view("guest.home");
